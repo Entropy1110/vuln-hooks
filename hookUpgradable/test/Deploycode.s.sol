2f07b347d29e4193e0b1b3000840a1f41ed45123
Entropy1110 <entropy1110@gmail.com>
2024-10-09 17:06:24 +0900

    modified deployment code

--- hookUpgradable/test/Deploycode.s.sol
+++ hookUpgradable/test/Deploycode.s.sol
@@ -1,35 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
 import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-import {TickMath} from "v4-core/src/libraries/TickMath.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
-import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
-import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
-
-import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
-import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
-import {EasyPosm} from "./utils/EasyPosm.sol";
-import {Fixtures} from "./utils/Fixtures.sol";
-import {Constants} from "v4-core/test/utils/Constants.sol";
 
 import {ProxyContract} from "../src/ProxyContract.sol";
 import {Implementation} from "../src/Implementation.sol";
 import {HookMiner} from "./utils/HookMiner.sol";
-import {Script} from "forge-std/Script.sol";
+import "forge-std/Script.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CounterTest is Script, Fixtures {
-    using EasyPosm for IPositionManager;
-    using PoolIdLibrary for PoolKey;
-    using CurrencyLibrary for Currency;
-    using StateLibrary for IPoolManager;
+contract CounterTest is Script {
+    uint160 public constant SQRT_PRICE_1_1 = 79228162514264337593543950336;
+    bytes constant ZERO_BYTES = new bytes(0);
 
     PoolId poolId;
     address payable mockAddr;
@@ -39,12 +26,21 @@ contract CounterTest is Script, Fixtures {
     int24 tickLower;
     int24 tickUpper;
 
-    function setUp() public {
-        // creates the pool manager, utility routers, and test tokens
-        manager = IPoolManager(0xE8E23e97Fa135823143d6b9Cba9c699040D51F70);
+    Currency currency0;
+    Currency currency1;
 
-        Token0 token0 = new Token0();
-        Token1 token1 = new Token1();
+    PoolKey key;
+    IPoolManager manager;
+
+
+    function run() public {
+        vm.startBroadcast();
+        // creates the pool manager, utility routers, and test tokens
+        manager = IPoolManager(0x39BF2eFF94201cfAA471932655404F63315147a4);
+        address deployer = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+
+        Token0 token0 = Token0(0x6f0cD9aC99c852bDBA06F72db93078cbA80A32F5);
+        Token1 token1 = Token1(0x8dB7EFd30A632eD236eAbde82286551f843D5487);
         
         currency0 = Currency.wrap(address(token0));
         currency1 = Currency.wrap(address(token1));
@@ -52,20 +48,23 @@ contract CounterTest is Script, Fixtures {
 
 
         bytes memory implBytecode = type(Implementation).creationCode;
-        (address addr, bytes32 salt) = HookMiner.find(address(this), Hooks.ALL_HOOK_MASK, implBytecode, "");
+        (address addr, bytes32 salt) = HookMiner.find(deployer, Hooks.ALL_HOOK_MASK, implBytecode, "");
         //create2 hookAddr
-        assembly {
-            addr := create2(0, add(implBytecode, 32), mload(implBytecode), salt)
-        }
+        // assembly {
+        //     addr := create2(0, add(implBytecode, 32), mload(implBytecode), salt)
+        // }
+        addr = address(new Implementation{salt : salt}());
         hookAddr = payable(addr);
 
 
         bytes memory mockBytecode = type(ProxyContract).creationCode;
-        (address addr2, bytes32 mockSalt) = HookMiner.find(address(this), Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG, mockBytecode, "");
+        (address addr2, bytes32 mockSalt) = HookMiner.find(deployer, Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG, mockBytecode, "");
 
-        assembly {
-            addr2 := create2(0, add(mockBytecode, 32), mload(mockBytecode), mockSalt)
-        }
+        // assembly {
+        //     addr2 := create2(0, add(mockBytecode, 32), mload(mockBytecode), mockSalt)
+        // }
+
+        addr2 = address(new ProxyContract{salt : mockSalt}());
         mockAddr = payable(addr2);
 
         ProxyContract(mockAddr).setImplementation(hookAddr);
@@ -74,32 +73,28 @@ contract CounterTest is Script, Fixtures {
         // Create the pool
         key = PoolKey(currency0, currency1, 3000, 60, IHooks(mockAddr));
         poolId = key.toId();
-        manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
+        manager.initialize(key, SQRT_PRICE_1_1, "");
 
-        // Provide full-range liquidity to the pool
-        tickLower = TickMath.minUsableTick(key.tickSpacing);
-        tickUpper = TickMath.maxUsableTick(key.tickSpacing);
+        // // Provide full-range liquidity to the pool
+        // tickLower = TickMath.minUsableTick(key.tickSpacing);
+        // tickUpper = TickMath.maxUsableTick(key.tickSpacing);
 
-        uint128 liquidityAmount = 100e18;
+        // uint128 liquidityAmount = 100e18;
 
-        (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
-            SQRT_PRICE_1_1,
-            TickMath.getSqrtPriceAtTick(tickLower),
-            TickMath.getSqrtPriceAtTick(tickUpper),
-            liquidityAmount
-        );
+        // (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
+        //     SQRT_PRICE_1_1,
+        //     TickMath.getSqrtPriceAtTick(tickLower),
+        //     TickMath.getSqrtPriceAtTick(tickUpper),
+        //     liquidityAmount
+        // );
 
-        (tokenId,) = posm.mint(
-            key,
-            tickLower,
-            tickUpper,
-            liquidityAmount,
-            amount0Expected + 1,
-            amount1Expected + 1,
-            address(this),
-            block.timestamp,
-            ZERO_BYTES
-        );
+        console.log("Hook Address: ", hookAddr);
+        console.log("Proxy Address: ", mockAddr);
+        console.log("Token0 Address: ", address(token0));
+        console.log("Token1 Address: ", address(token1));
+
+
+
     }
 }
 
@@ -108,10 +103,18 @@ contract Token0 is ERC20 {
         _mint(msg.sender, 1000000 * (10 ** uint(decimals())));
     }
 
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
 }
 
 contract Token1 is ERC20 {
     constructor () ERC20 ("Token1", "TK1") {
         _mint(msg.sender, 1000000 * (10 ** uint(decimals())));
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
     }
 }
